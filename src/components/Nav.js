0243463bd0ba6a6120e8f4debb40bdeac1ede759
FlorentCRd <fc.cordier83@gmail.com>
2023-03-09 08:33:22 +0100

    commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,22 @@
 import React from "react";
+//import icons
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+//import Link
+import { Link } from "react-scroll";
 
 const Nav = () => {
-    return <div>Nav</div>
+  return (
+    <nav>
+      <div className="container mx-auto">
+        <div>
+          <Link>
+            <BiHomeAlt />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
